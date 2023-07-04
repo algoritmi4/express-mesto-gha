d43222cd8e45c954c279425f72031cb574423268
Ivan <ButorinLP@yandex.ru>
2023-07-04 05:37:55 +0300

    test5

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ router.patch('/me', celebrate({
 }), updateProfile);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().pattern('https?://[a-zA-Z0-9-._~:/?#[]@!$&\'()*+,;=]+'),
+    avatar: Joi.string().required().hostname(),
   }),
 }), updateAvatar);
 
